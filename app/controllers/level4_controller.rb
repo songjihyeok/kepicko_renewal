9a46c3f6f39763224cb86b028fcefe21191f0b99
songjihyeok <porr3444@naver.com>
2020-11-10 17:24:27 +0900

    china bug fix

--- app/controllers/level4_controller.rb
+++ app/controllers/level4_controller.rb
@@ -4,7 +4,7 @@ class Level4Controller < ApplicationController
     @level3_id = Package.find_by(level4_id: @level4.id).level3_id
     @level2_id = System.find_by(level3_id: @level3_id).level2_id
     @level1_id = Component.find_by(level2_id: @level2_id).level1_id
-    @level4s = Package.where(level3_id: @level3_id){ |i| Level4.find(i.level4_id)}
-    @level5s = Product.where(level4_id: @level4.id){ |i| Level5.find(i.level5_id)}
+    @level4s = Package.where(level3_id: @level3_id).map{ |i| Level4.find(i.level4_id)}
+    @level5s = Product.where(level4_id: @level4.id).map{ |i| Level5.find(i.level5_id)}
   end
 end
