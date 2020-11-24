a543a3a7fb7674d39fb9645e8d06f8524b362e00
songjihyeok <porr3444@naver.com>
2020-11-24 20:04:53 +0900

    level4

---
+++ db/migrate/20201124110355_add_reference.rb
@@ -0,0 +1,8 @@
+class AddReference < ActiveRecord::Migration[6.0]
+  def change
+        remove_reference :skill_firsts, :link
+        remove_reference :skill_seconds, :link
+        add_reference :skill_firsts, :level4, index: true
+        add_reference :skill_seconds, :level4, index: true
+  end
+end
