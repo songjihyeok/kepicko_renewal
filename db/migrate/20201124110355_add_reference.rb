b3b0468cc3f3604673f54ae4ec5d2bd2cac40c76
songjihyeok <porr3444@naver.com>
2021-07-16 15:42:30 +0900

    english

--- db/migrate/20201124110355_add_reference.rb
+++ db/migrate/20201124110355_add_reference.rb
@@ -2,7 +2,7 @@ class AddReference < ActiveRecord::Migration[6.0]
   def change
         remove_reference :skill_firsts, :link
         remove_reference :skill_seconds, :link
-        add_reference :skill_firsts, :level4, index: true
-        add_reference :skill_seconds, :level4, index: true
+        # add_reference :skill_firsts, :level4, index: true
+        # add_reference :skill_seconds, :level4, index: true
   end
 end
